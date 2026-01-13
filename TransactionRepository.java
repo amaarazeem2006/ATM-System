1056ed8b60e5886bfa479790c27ea01060c0eef2
amaarazeem2006 <amaarmemon42@gmail.com>
2026-01-14 00:44:02 +0500

    Create TransactionRepository

---
+++ TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.aur.atm.repository;
+
+import com.aur.atm.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+    List<Transaction> findByUserId(int userId);
+}
